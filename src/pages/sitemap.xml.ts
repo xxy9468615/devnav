2dc79b43c99595d9ffcaa7dc864017cb97d62b33
xxy9468615 <63351664+xxy9468615@users.noreply.github.com>
2026-06-27 04:09:08 +0800

    feat: add sitemap.xml endpoint and robots.txt

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,28 @@
+import type { APIRoute } from 'astro';
+import { SITE } from '../../config/site';
+import { loadCategories } from '../../lib/resources';
+
+export const GET: APIRoute = async () => {
+  const categories = await loadCategories();
+
+  const urls: string[] = [
+    SITE.url,
+    ...categories.map(c => `${SITE.url}/${c.id}`),
+  ];
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls.map(url => `  <url>
+    <loc>${url}</loc>
+    <changefreq>daily</changefreq>
+    <priority>0.8</priority>
+  </url>`).join('\n')}
+</urlset>`;
+
+  return new Response(xml, {
+    headers: {
+      'Content-Type': 'application/xml',
+      'Cache-Control': 'public, max-age=3600',
+    },
+  });
+};
